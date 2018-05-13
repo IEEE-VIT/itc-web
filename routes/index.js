60aab820280a4478abe7c1990f1a56d5a31d2fdf
devil-Coder <raj.chandra2016@vitstudent.ac.in>
2018-05-13 15:17:16 +0530

    added itinerary

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* GET Itinerary page. */
+router.get('/itinerary', function(req, res, next) {
+    res.render('itinerary');
+});
+
 
 module.exports = router;
