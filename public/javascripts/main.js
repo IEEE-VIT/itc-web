04e6ad62d6e1cb867bb72487a2ae345747c4f30c
Heetarthi Kamdar <35897737+heetarthikamdar@users.noreply.github.com>
2018-06-21 16:14:10 +0530

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -266,3 +266,23 @@ jQuery(document).ready(function($){
 		return window.getComputedStyle(document.querySelector('.cd-horizontal-timeline'), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "");
 	}
 });
+
+
+/*Speakers Page*/
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal 
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
