002728114c59eb1f3221b5f629e398da12358965
Heetarthi Kamdar <35897737+heetarthikamdar@users.noreply.github.com>
2018-06-21 23:38:08 +0530

    Update main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -268,21 +268,3 @@ jQuery(document).ready(function($){
 });
 
 
-/*Speakers Page*/
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal 
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-    modal.style.display = "none";
-}
